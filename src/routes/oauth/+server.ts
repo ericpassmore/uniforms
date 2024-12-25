96e1252f6301210e0ad9fb933bdb016537426bd4
Eric Passmore <eric.passmore@gmail.com>
2024-12-25 01:00:16 -0800

    teamsanp oauth requires url params

--- src/routes/oauth/+server.ts
+++ src/routes/oauth/+server.ts
@@ -1,45 +1,39 @@
-import { redirect, json  } from '@sveltejs/kit';
+import { error, redirect, json} from '@sveltejs/kit';
 import { OAUTH_CLIENT_ID,
     OAUTH_CLIENT_SECRET,
     OAUTH_REDIRECT_URI,
     OAUTH_TOKEN_URL  } from '$env/static/private'
 
-export async function GET({ cookies, request }) {
-    const { code, state } = await request.json();
+export const GET = async({ cookies, url }) => {
+
+    const code = url.searchParams.get('code')
+    const state = url.searchParams.get('state')
 
     if (!code) {
-        return json({ error: 'No code provided' }, { status: 400 });
+        error(500,{ message: 'No code provided' });
     }
 
     if (!state || state !== 'hamburger') {
-        console.error('Bad State on Authorization:');
-        return json({ error: 'Unexpected state code' }, { status: 500 });
+        error(500,{ message: 'Unexpected state code' });
     }
-    console.log("Got Token and state")
-    try {
 
-        const response = await fetch(OAUTH_TOKEN_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: new URLSearchParams({
-                code: code,
-                client_id: OAUTH_CLIENT_ID,
-                client_secret: OAUTH_CLIENT_SECRET,
-                redirect_uri: OAUTH_REDIRECT_URI,
-                grant_type: 'authorization_code',
-            }).toString(),
+    const token_exchange_url = OAUTH_TOKEN_URL +
+        `?code=${code}` +
+        `&client_id=${OAUTH_CLIENT_ID}` +
+        `&client_secret=${OAUTH_CLIENT_SECRET}` +
+        `&redirect_uri=${encodeURIComponent(OAUTH_REDIRECT_URI)}` +
+        'grant_type=authorization_code';
+
+    try {
+        const response = await fetch(token_exchange_url, {
+            method: 'POST'
         });
 
         if (!response.ok) {
             const errorData = await response.json();
-            console.error('Error exchanging code for token:', errorData);
-            return json({ error: 'Token Exchange Failed' }, { status: 500 });
+            error(500, { message: `Token Exchange Failed with ${errorData}` });
         }
 
-        console.log("fetching access token")
         const { access_token } = await response.json();
 
         // suggest add httpOnly: true  secure: true sameSite: 'strict'
@@ -50,10 +44,9 @@ export async function GET({ cookies, request }) {
                 sameSite: true,
                 maxAge: 60 * 60 * 24 * 7 // 1 week
             });
-            redirect(307, '/admin');
+            redirect(307, '/');
 
-    } catch (error) {
-        console.error('Unexpected error:', error);
-        return json({ error: 'Unexpected error occurred' }, { status: 500 });
+    } catch (error_obj) {
+        error(500, { message: `Unexpected error:${error_obj}` });
     }
 }
